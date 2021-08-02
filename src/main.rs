b234e10f7ab598b4abac35b556fbbcc2ca7b1976
404b0tnet <jsmith728.js@gmail.com>
2021-08-02 16:53:56 -0400

    Commented what HttpServer and App are

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
-/*async fn greet(req: HttpRequest) -> impl Responder {
+
+/*
+A type implements the Responder trait if it can be converted into a HttpResponse
+
+async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
-} */
+} 
+*/
+
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
@@ -11,12 +17,15 @@ async fn health_check() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // HttpServer handles all transport layer concerns
+    // App is the application builder - all application logic
     HttpServer::new(|| {
         /*App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet)) */
         
         App::new()
+        // call with curl http://127.0.0.1:8000/health_check
             .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
